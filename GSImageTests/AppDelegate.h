9d2f4488557d82e53574db0a8ec9d35150b9a41a
Gregory John Casamento <greg.casamento@gmail.com>
2025-11-01 09:30:55 -0400

    Initial Commit

---
+++ GSImageTests/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GSImageTests
+//
+//  Created by Gregory Casamento on 11/1/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GSImageTests/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  GSImageTests
+//
+//  Created by Gregory Casamento on 11/1/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
